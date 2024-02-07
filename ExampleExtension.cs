0f890e979e31663f8bf52df764e1f21c117da643
Jens van de Wiel <jens.vdwiel@gmail.com>
2024-02-07 20:35:24 +0100

    Big Bang

---
+++ ExampleExtension.cs
@@ -0,0 +1,57 @@
+﻿using Oxide.Core;
+using Oxide.Core.Extensions;
+using Oxide.Core.Logging;
+using Oxide.Core.Plugins;
+using Oxide.Ext.ExampleExtension.Libraries;
+
+namespace Oxide.Ext.ExampleExtension
+{
+    public class ExampleExtension : Extension
+    {
+        public ExampleExtension(ExtensionManager manager) : base(manager)
+        {
+            _logger = manager.Logger;
+        }
+
+        public override string Name => "ExampleExtension";
+        public override string Author => "John Doe";
+        public override VersionNumber Version => new VersionNumber(6, 9, 0);
+        
+        private Logger _logger;
+        
+        /// <summary>
+        /// Called when the extension is created by the extension manager
+        /// </summary>
+        public override void Load()
+        {
+            _logger.Write(LogType.Info, "ExampleExtension: Load()");
+        }
+
+        
+        /// <summary>
+        /// Called when the extension is unloaded by the extension manager
+        /// </summary>
+        public override void Unload()
+        {
+            _logger.Write(LogType.Info, "ExampleExtension: Unload()");
+        }
+        
+        /// <summary>
+        /// Called when a mod is loaded from extension manager
+        /// </summary>
+        public override void OnModLoad()
+        {
+            _logger.Write(LogType.Error, "ExampleExtension: OnModLoad()");
+            // Register a library with the extension manager
+            Manager.RegisterLibrary(nameof(ExampleLibrary), new ExampleLibrary());
+        }
+
+        /// <summary>
+        /// Called when the server is shutting down
+        /// </summary>
+        public override void OnShutdown()
+        {
+            _logger.Write(LogType.Warning, "ExampleExtension: OnShutdown()");
+        }
+    }
+}
---
+++ Libraries/ExampleLibrary.cs
@@ -0,0 +1,16 @@
+﻿using Oxide.Core.Libraries;
+
+namespace Oxide.Ext.ExampleExtension.Libraries
+{
+    public class ExampleLibrary: Library
+    {
+        [LibraryFunction(nameof(Add))]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        
+        [LibraryProperty(nameof(Foo))]
+        public string Foo => "Bar";
+    }
+}
